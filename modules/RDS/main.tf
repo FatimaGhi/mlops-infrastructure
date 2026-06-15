950882903d2ca7db198f31cdbf8910b1fffe515e
FatimaGhi <fatimazahraeelghiouan69@gmail.com>
2026-06-15 12:16:08 +0100

    fix: allow RDS access from VPC CIDR for EKS pods

--- modules/RDS/main.tf
+++ modules/RDS/main.tf
@@ -71,3 +71,25 @@ resource "aws_db_instance" "mlflow" {
     Project = "mlops"
   }
 }
+resource "aws_security_group" "rds" {
+  name   = "mlflow-rds-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "mlflow-rds-sg"
+  }
+}
